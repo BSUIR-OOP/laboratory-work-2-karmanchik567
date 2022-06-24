41b612c5dc2a29baba5271128abe88d791971fa1
karmanchik567 <fedukevicvlad@gmail.com>
2022-06-24 21:51:58 +0300

    v2

---
+++ OOP2/Models/BaseFigure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace lab2.Figures
+{
+    public abstract class BaseFigure
+    {
+        public abstract void drawCanvas(Canvas canvas);
+
+        public static System.Windows.Shapes.Line drawLine(int x1, int x2, int y1, int y2)
+        {
+            var Line = new System.Windows.Shapes.Line();
+            Line.Stroke = System.Windows.Media.Brushes.Black;
+            Line.X1 = x1;
+            Line.X2 = x2;
+            Line.Y1 = y1;
+            Line.Y2 = y2;
+            Line.HorizontalAlignment = HorizontalAlignment.Left;
+            Line.VerticalAlignment = VerticalAlignment.Center;
+            Line.StrokeThickness = 3;
+            return Line;
+        }
+
+    }
+}
